b8f20f83f1deaa8aa304672888f1f611b1b19d33
sevenkztang <sevenkztang@tencent.com>
2025-04-01 14:41:47 +0800

    fix: google fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Inter, Tangerine, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
-export const tangerine = Tangerine({weight: '400'});
+export const tangerine = Tangerine({weight: '400', subsets: ['latin']});
 
 export const lusitana = Lusitana({
   weight: ['400', '700'],
