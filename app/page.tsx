f4a6a0e4f85cd29e52fba9a36541206555b1c90f
sevenkztang <sevenkztang@tencent.com>
2025-04-08 19:04:25 +0800

    feat: eslint config

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import { tangerine } from '@/app/ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { tangerine } from '@/app/ui/fonts'
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Page() {
+  // lint-stage测试
+  // eslint-disable-next-line no-console
+  console.log('=====');
+
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
+      {/* <a href="/"></a> */}
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={tangerine.className}>Tangerine字体</div>
